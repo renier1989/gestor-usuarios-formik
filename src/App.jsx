56bfe896e1abcabf785475befdd21944b5e1ca30
Renier Vargas <renier1989@gmail.com>
2023-08-11 05:31:55 -0500

    Class84 - Introduccion a Formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useFormik } from "formik";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      lastname: "",
+      email: "",
+    },
+    onSubmit: (values) => console.log(values),
+  });
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="h-screen bg-gray-200 flex flex-col items-center justify-center">
+      <div className="bg-white p-10 rounded-md w-[700px]">
+        <form onSubmit={formik.handleSubmit}>
+          <div className="w-full">
+            <label className="font-semibold text-gray-500 text-lg"> Nombre : </label>
+            <input
+              className="w-full border-[1px] border-gray-400 rounded-md px-2 py-1 outline-none mb-5 mt-2"
+              name="name"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.name}
+              autoComplete="off"
+            />
+          </div>
+          <div>
+            <label className="font-semibold text-gray-500 text-lg"> Apellido : </label>
+            <input
+            className="w-full border-[1px] border-gray-400 rounded-md px-2 py-1 outline-none mb-5 mt-2"
+              name="lastname"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.lastname}
+              autoComplete="off"
+            />
+          </div>
+          <div>
+            <label className="font-semibold text-gray-500 text-lg"> Correo : </label>
+            <input
+            className="w-full border-[1px] border-gray-400 rounded-md px-2 py-1 outline-none mb-5 mt-2"
+              name="email"
+              type="text"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              autoComplete="off"
+            />
+          </div>
+          <div className="flex items-center justify-center">
+            <button className="px-4 py-2 bg-green-600 rounded-md font-semibold text-white" type="submit">Enviar</button>
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
